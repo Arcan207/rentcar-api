2927a0371ab2f4e68b5942bbdf3451f2b709e9d1
Arcan207 <aryacandra13@gmail.com>
2022-11-07 01:37:32 +0800

    tambah relasi database

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,7 +17,7 @@ class UserSeeder extends Seeder
     {
         User::create([
             "name" => "admin",
-            "email" => "admin@gnail.com",
+            "email" => "admin@gmail.com",
             "password" => bcrypt("admin123"),
         ]);
     }
